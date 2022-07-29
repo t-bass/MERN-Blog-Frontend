ff01ca1efb94c0eb6253bfab2652eaab252d4fbf
Виктор Гончаров <t-bass@mail.ru>
2022-07-30 00:46:57 +0300

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,6 @@ export const Home = () => {
                 id={obj._id}
                 title={obj.title}
                 imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
-                // imageUrl='https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png'
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
